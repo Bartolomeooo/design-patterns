546e10fa8bd27f151298fcb5a57bd50d0eca80f1
Bartolomeooo <bartoszkawiak@gmail.com>
2024-12-10 18:19:27 +0100

    Add OrderCreatedNotification logic

--- src/OrderCreatedNotification.java
+++ src/OrderCreatedNotification.java
@@ -5,7 +5,8 @@ public class OrderCreatedNotification implements Notification {
 	 * @param status
 	 */
 	public void display(String status) {
-		System.out.println("Zamowienie zostalo utworzone, jego status: " + status);
+		if (status == "Canceled") System.out.println("Zamowienie zostalo anulowane ze wzgledu na wymagania niemozliwe do spelenienia, jego status: " + status);
+		else System.out.println("Zamowienie zostalo utworzone, jego status: " + status);
 	}
 
 }
